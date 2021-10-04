49e051bf157173e3b8ae29e4ec35f9d6f2a2566a
PatriciaTorresGraciano <patricia.dstorres@hotmail.com>
2021-10-04 20:40:02 -0300

    Criando repository de Usuario

---
+++ src/main/java/org/generation/blogPessoal/repositories/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package org.generation.blogPessoal.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.generation.blogPessoal.models.UsuarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long>{
+
+	public Optional<UsuarioModel> findByEmail(String email);
+	
+	public List<UsuarioModel> findAllByNomeContainingIgnoreCase(String nome);
+}
