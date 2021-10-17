3772d2ecc689565ba140e738a1ecba1e1c8d77d9
PatriciaTorresGraciano <patricia.dstorres@hotmail.com>
2021-10-17 16:54:52 -0300

    Adicionando UserDetailsImplements na camada security

--- src/main/java/org/generation/blogPessoal/models/UsuarioLogin.java
+++ src/main/java/org/generation/blogPessoal/models/UsuarioLogin.java
@@ -2,4 +2,48 @@ package org.generation.blogPessoal.models;
 
 public class UsuarioLogin {
 
+	private Long idUsuario;
+	private String nome;
+	private String usuario;
+	private String login;
+	private String senha;
+	private String token;
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
 }
--- src/main/java/org/generation/blogPessoal/repositories/UsuarioRepository.java
+++ src/main/java/org/generation/blogPessoal/repositories/UsuarioRepository.java
@@ -1,6 +1,5 @@
 package org.generation.blogPessoal.repositories;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.generation.blogPessoal.models.UsuarioModel;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long>{
 
-	public Optional<UsuarioModel> findByEmail(String email);
+	public Optional<UsuarioModel> findByUsuario(String usuario);
 	
-	public List<UsuarioModel> findAllByNomeContainingIgnoreCase(String nome);
 }
